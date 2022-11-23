78d0826a445891d3ba043ee73a3c12c2c976f385
Kenlan2022 <rando.au@gmail.com>
2022-11-23 11:16:59 +0800

    1

---
+++ 專案3/datasource.py
@@ -0,0 +1,17 @@
+import requests
+
+
+api_key = "1b9553d7078c05ee58c7baa6a6930fe6"
+cityName = "Taipei"
+
+
+def get_forecase_data():
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={cityName},tw&APPID={api_key}&lang=zh_tw&units=metric"
+
+    response = requests.get(url=url)
+
+    if response.ok:
+        print("下載成功")
+        print("response.txt")
+    else:
+        print("下載失敗")
--- 專案3/main.py
+++ 專案3/main.py
@@ -1,5 +1,9 @@
+import datasource
+
+
 def main():
     print("這裡是main function")
+    datasource.get_forecase_data()
 
 
 if __name__ == "__main__":
