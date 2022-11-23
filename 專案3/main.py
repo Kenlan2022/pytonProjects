4dcc768c11bc597a31f66ca31128070c45d1d1cc
Kenlan2022 <rando.au@gmail.com>
2022-11-23 16:29:31 +0800

    1

--- 專案3/main.py
+++ 專案3/main.py
@@ -4,12 +4,23 @@ import tkinter as tk
 
 
 class Window(tk.Tk):
-    def __init__(self):
+    def __init__(self, cities_dict):
         super().__init__()
+        tk.Label(self, text="各縣市4天天氣預測", font=(
+            '標楷體', 20)).pack(padx=30, pady=30)  # 標題不能改
+
+        # 建立存放按鈕的容器
+        # button_frame = tk.Frame(self).pack() 這樣會先pack，傳出none
+        button_frame = tk.Frame(
+            self, background="#cccccc", width=200, height=300)
+        button_frame.pack()
+
+        for key in cities_dict:
+            tk.Button(button_frame, text=key).pack(side=tk.LEFT)
 
 
 def main():
-    window = Window()
+    window = Window(ds.tw_county_names)
     window.title("各縣市4天天氣預測")
     window.mainloop()
 
