9d9fd11a7017519649b506d9afec85fc5cb29897
Kenlan2022 <rando.au@gmail.com>
2022-11-23 13:40:06 +0800

    1

--- 專案3/main.py
+++ 專案3/main.py
@@ -3,8 +3,10 @@ import datasource
 
 def main():
     print("這裡是main function")
-    allData = datasource.get_forecase_data()
-    print(allData)
+    list_data = datasource.get_forecase_data()
+    for item in list_data:
+        print(item["main"]["temp"])
+
 
 if __name__ == "__main__":
     print("這裡是程式的執行點")
