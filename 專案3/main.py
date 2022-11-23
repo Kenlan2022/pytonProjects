ee8df78a8a1701a53a6388109acb2a5c8b7c6ceb
Kenlan2022 <rando.au@gmail.com>
2022-11-23 15:33:54 +0800

    1

--- 專案3/main.py
+++ 專案3/main.py
@@ -1,19 +1,27 @@
 import datasource as ds
 from secret import api_key
+import tkinter as tk
+
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
 
 def main():
-    print("這裡是main function")
-    try:
-        list_data = ds.get_forecase_data(ds.tw_county_names["金門"], api_key)
-    except Exception as e:
-        print(e)
-        return
+    window = Window()
+    window.title("各縣市4天天氣預測")
+    window.mainloop()
 
-    for item in list_data:
-        print(item["main"]["temp"])
+    # try:
+    #     list_data = ds.get_forecase_data(ds.tw_county_names["金門"], api_key)
+    # except Exception as e:
+    #     print(e)
+    #     return
+
+    # for item in list_data:
+    #     print(item["main"]["temp"])
 
 
 if __name__ == "__main__":
-    print("這裡是程式的執行點")
     main()
